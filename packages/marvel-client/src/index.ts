c08e7ef1401cc81f35e16f6a308d3bf643342b13
Ramzi <ramzi.hannachi1991@gmail.com>
2022-10-17 23:17:13 +0200

    refacto: rename httpClient to http-client

--- packages/marvel-client/src/index.ts
+++ packages/marvel-client/src/index.ts
@@ -1,26 +1,11 @@
-import {fetchComics, fetchCreatorsByComicId, IComic} from "marvel-services";
+import {fetchComics, fetchCreatorsByComicId} from "marvel-services";
 
 const main = async () => {
-
     const comicsResponse = await fetchComics()
-
-    if (comicsResponse.status === 'OK') {
-        // TODO array of comic Ids
-        const comics = comicsResponse.response.data.results.slice(0,3) // TODO remove slice
-        const comicIdsTODO = comics.map(async (comic: IComic) => {
-            const creatorsResponse = await fetchCreatorsByComicId(comic.id)
-            if (creatorsResponse.status === "OK") {
-                return creatorsResponse.response.data.results
-            }
-            return
-        })
-
-        const result = await Promise.all(comicIdsTODO)
-        console.log('array of comic Ids ===>', JSON.stringify(result, null, 2))
-    }
+    const fetchCreators = await fetchCreatorsByComicId(323)
+    console.log(JSON.stringify(comicsResponse, null, 2))
+    console.log(JSON.stringify(fetchCreators, null, 2))
 }
-
-
 
 (async () => {
     try {
